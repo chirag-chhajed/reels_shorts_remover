2ead3413c6aee03d8d960388d66046058ac81b4b
Chirag Chhajed <chhajedchirag18@gmail.com>
2024-01-22 16:27:54 +0530

    some change

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,47 +1,53 @@
 console.log("hello from content");
 
 // Your code to manipulate the DOM goes here
+const observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function () {
+    // console.log("DOM changed!");
+    // Your code here
+    const reelsRemover =
+      document.querySelector<HTMLAnchorElement>('a[href="/reels/"]');
 
-const reelsRemover =
-  document.querySelector<HTMLAnchorElement>('a[href="/reels/"]');
+    if (reelsRemover) {
+      reelsRemover.parentNode?.removeChild(reelsRemover);
+    }
 
-console.log(reelsRemover, "button reel");
+    const threadsIconRemover = document.querySelector<HTMLAnchorElement>(
+      'a[href="https://www.threads.net/"]'
+    );
 
-if (reelsRemover) {
-  reelsRemover.parentNode?.removeChild(reelsRemover);
-}
+    // console.log(threadsIconRemover, "button threads");
+    if (threadsIconRemover) {
+      threadsIconRemover.parentNode?.removeChild(threadsIconRemover);
+    }
 
-const threadsIconRemover = document.querySelector<HTMLAnchorElement>(
-  'a[href="https://www.threads.net/"]'
-);
+    const youTubeShorts =
+      document.querySelector<HTMLAnchorElement>('a[title="Shorts"]');
 
-console.log(threadsIconRemover, "button threads");
-if (threadsIconRemover) {
-  threadsIconRemover.parentNode?.removeChild(threadsIconRemover);
-}
+    if (youTubeShorts) {
+      youTubeShorts.parentNode?.removeChild(youTubeShorts);
+    }
 
-const youTubeShorts =
-  document.querySelector<HTMLAnchorElement>('a[title="Shorts"]');
+    // content.js
+    chrome.runtime.sendMessage({ text: "getActiveTabId" }, function (response) {
+      const tabUrl: string = response?.tabId?.url;
 
-if (youTubeShorts) {
-  youTubeShorts.parentNode?.removeChild(youTubeShorts);
-}
+      //   console.log("Active tab URL is", tabUrl);
 
-// content.js
-chrome.runtime.sendMessage({ text: "getActiveTabId" }, function (response) {
-  const tabUrl: string = response?.tabId?.url;
-
-  console.log("Active tab URL is", tabUrl);
-
-  // Check if the tab URL starts with "https://www.instagram.com/reels/"
-  if (
-    tabUrl &&
-    (tabUrl.startsWith("https://www.instagram.com/reels/") ||
-      tabUrl.startsWith("https://www.youtube.com/shorts/"))
-  ) {
-    chrome.runtime.sendMessage({
-      text: "navigate",
-      url: "https://www.google.com",
+      // Check if the tab URL starts with "https://www.instagram.com/reels/"
+      if (
+        tabUrl &&
+        (tabUrl.startsWith("https://www.instagram.com/reels/") ||
+          tabUrl.startsWith("https://www.youtube.com/shorts/") ||
+          tabUrl.startsWith("https://www.instagram.com/reel/") ||
+          tabUrl.startsWith("https://instagram.com/p/"))
+      ) {
+        chrome.runtime.sendMessage({
+          text: "navigate",
+          url: "https://www.google.com",
+        });
+      }
     });
-  }
+  });
 });
+observer.observe(document.body, { childList: true, subtree: true });
